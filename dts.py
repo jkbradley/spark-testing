bc761a1fc7189e650c01064bb7cd5317f66165b1
Joseph K. Bradley <joseph.kurata.bradley@gmail.com>
2014-10-10 12:02:08 -0700

    added dt test

---
+++ dts.py
@@ -0,0 +1,67 @@
+"""
+Test script for Decision Trees and Random Forests
+
+Run with a range of parameters.
+Use DecisionTreeRunner, which prints parameters, accuracy, and timing results to STDOUT.
+The output file is named: "dts-[testTag]-[date-time].log"
+"""
+
+import os, re, time
+
+# Test settings
+testTag = "avoid-agg"
+dataset = "mnist"
+maxDepths = [5, 10]
+numTreess = [1, 10]
+fracTest = 0.2
+logdir = "/root/mnist-logs"
+
+dieOnFailure = True
+dryRun = True  # print what will run, but do not run anything
+
+# Datasets
+if dataset == "mnist":
+    trainFilePath = "s3n://databricks-mllib/mnist8m-libsvm"
+    testFilePath = "s3n://databricks-meng/mllib-data/mnist-digits/mnist-digits-test.txt"
+else:
+    trainFilePath = dataset
+
+# Run stuff
+sparkDir = "/root/spark"
+
+def getMasterURI():
+    f = open(sparkDir + "/conf/spark-env.sh")
+    re_pattern = re.compile("export SPARK_MASTER_IP=(.+)")
+    masterURI = ""
+    for line in f:
+        line = line.strip()
+        match = re_pattern.match(line)
+        if match:
+            masterURI = math.group(0)
+            break
+    f.close()
+    return masterURI
+
+masterURI = getMasterURI()
+
+for maxDepth in maxDepths:
+    for numTrees in numTreess:
+        timeStamp = time.strftime("%Y-%m-%d_%H:%M:%S")
+        outPath = "dts-%s-maxDepth%d-numTrees%d-%s.log" % (testTag, maxDepth, numTrees, timeStamp)
+        runString = "time /root/spark/bin/spark-submit --class org.apache.spark.examples.mllib.DecisionTreeRunner" \
+          + " --master spark://" + masterURI + ":7077 --driver-memory 20g" \
+          + " /root/spark-git/examples/target/scala-2.10/spark-examples-1.2.0-SNAPSHOT-hadoop1.0.4.jar" \
+          + (" %s --testInput %s" % (trainFilePath, testFilePath)) \
+          + (" --maxDepth %d --numTrees %d --fracTest %g" % (maxDepth, numTrees, fracTest)) \
+          + (" &> %s" % outPath)
+        println("RUNNING: " + runString)
+        if not dryRun and os.system(runString) != 0:
+            println("FAILED TEST: " + runString)
+            try:
+                os.rename(outPath, outPath + ".FAILED")
+                println("RENAMED FAILED TEST LOG: " + outPath + ".FAILED")
+            except:
+                println("COULD NOT RENAME FAILED TEST LOG: " + outPath)
+            if dieOnFailure:
+                raise Exception("Failed during test.")
+
